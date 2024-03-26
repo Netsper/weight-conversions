ff796cd574884f1c6018230b343ed3cf75791eee
Barış <baris@barisaksu.com>
2024-03-26 22:32:15 +0300

    feat: add conversion

--- src/WeightConversionsClass.php
+++ src/WeightConversionsClass.php
@@ -1,7 +1,23 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Netsper\WeightConversions;
 
 class WeightConversionsClass
 {
+    public static function kilograms(float $kilograms): self
+    {
+        return new static($kilograms);
+    }
+
+    public function __construct(
+        protected float $kilograms
+    ) {
+    }
+
+    public function toPound(): float
+    {
+        return $this->kilograms * 2.2046;
+    }
 }
